9f1273f5fcb4f3a6c9d06cc1ec9426e21ef991c1
Giovanni J <giovannyjunco@gmail.com>
2022-12-22 14:29:05 -0500

    Comienzo 1

---
+++ ej.py
@@ -0,0 +1,20 @@
+x=1
+while x <21:
+  if(x==8):
+    x+=1
+    continue
+  print(x, end=': ')
+  a=2
+  while a <x:     
+    if x%a==0:
+      print("no es primo")
+      break
+    a+=1
+  else:
+    print("es primo")
+  x+=1
+else:
+  print('fin Algorit')
+
+
+
